0f8adfd0f4fd87fc5d453e05a2ed7b4a73c644c4
trist <trist@LAPTOP-CP1EBFT0>
2023-04-23 16:59:41 +0200

    handle no sap in joblist

--- Source/Pogoman/Patches/JobGiver_AITrashBuildingsDistant.cs
+++ Source/Pogoman/Patches/JobGiver_AITrashBuildingsDistant.cs
@@ -7,14 +7,16 @@ using System.Text;
 using System.Threading.Tasks;
 using Verse.AI;
 using Verse;
+using static PogoAI.Patches.JobGiver_AISapper;
 
 namespace PogoAI.Patches
 {
     [HarmonyPatch(typeof(RimWorld.JobGiver_AITrashBuildingsDistant), "TryGiveJob")]
     static class JobGiver_AITrashBuildingsDistant_TryGiveJob_Patch
     {
-        static bool Prefix()
+        static bool Prefix(Pawn pawn, ref Job __result)
         {
+            JobGiver_AISapper_TryGiveJob_Patch.Prefix(pawn, ref __result);
             return false;                
         }
     }
