6cb078142669e923a39e12a32cd863890d018108
trist <trist@LAPTOP-CP1EBFT0>
2025-08-01 09:24:49 +0200

    switch to new ce turret class

--- Source/Pogoman/Patches/AvoidGrid.cs
+++ Source/Pogoman/Patches/AvoidGrid.cs
@@ -82,20 +82,24 @@ namespace PogoAI.Patches
                 {
                     if (allBuildingsColonist[i].def.building.ai_combatDangerous)
                     {
-                        CompEquippable equip;
+                        CompEquippable equip = null;
                         var threatCondition = false;
                         var building = allBuildingsColonist[i];
-                        if (Init.combatExtended && building.GetType().ToString() == "CombatExtended.Building_TurretGunCE")
+                        if (Init.combatExtended && building.GetType().ToString().Contains("CombatExtended"))
                         {
-                            equip = (CompEquippable)building.GetType().GetProperty("GunCompEq").GetValue(building, null);
-                            var active = (bool)building.GetType().GetProperty("Active").GetValue(building, null);
-                            var activePowerSource = (CompPowerTrader)building.GetType().GetProperty("PowerComp").GetValue(building, null);
-                            var currentTarget = (LocalTargetInfo)building.GetType().GetProperty("CurrentTarget").GetValue(building, null);
-                            var emptyMagazine = (bool)building.GetType().GetProperty("EmptyMagazine").GetValue(building, null);
-                            var isMannable = (bool)building.GetType().GetProperty("IsMannable").GetValue(building, null);
-                            var mannedByColonist = ((CompMannable)building.GetType().GetProperty("MannableComp").GetValue(building, null))?.MannedNow ?? false;
-                            threatCondition = (active || (activePowerSource?.PowerNet?.CanPowerNow(activePowerSource) ?? false))
-                                && currentTarget == null && !emptyMagazine && equip != null && (!runMannableCheck || !isMannable || mannedByColonist);
+                            try
+                            {
+                                equip = (CompEquippable)building.GetType().GetProperty("GunCompEq").GetValue(building, null);
+                                var active = (bool)building.GetType().GetProperty("Active").GetValue(building, null);
+                                var activePowerSource = (CompPowerTrader)building.GetType().GetProperty("PowerComp").GetValue(building, null);
+                                var currentTarget = (LocalTargetInfo)building.GetType().GetProperty("CurrentTarget").GetValue(building, null);
+                                var emptyMagazine = (bool)building.GetType().GetProperty("EmptyMagazine").GetValue(building, null);
+                                var isMannable = (bool)building.GetType().GetProperty("IsMannable").GetValue(building, null);
+                                var mannedByColonist = ((CompMannable)building.GetType().GetProperty("MannableComp").GetValue(building, null))?.MannedNow ?? false;
+                                threatCondition = (active || (activePowerSource?.PowerNet?.CanPowerNow(activePowerSource) ?? false))
+                                    && currentTarget == null && !emptyMagazine && equip != null && (!runMannableCheck || !isMannable || mannedByColonist);
+                            }
+                            catch (Exception) { }
                         }
                         else
                         {
@@ -107,7 +111,7 @@ namespace PogoAI.Patches
                                 && (!runMannableCheck || !building_TurretGun.IsMannable || Traverse.Create(building_TurretGun).Field("MannedByColonist").GetValue<bool>())
                                 && (building_TurretGun.refuelableComp?.HasFuel ?? true);
                         }
-                        if (threatCondition)
+                        if (threatCondition && equip != null)
                         {
                             PrintAvoidGridLOSThing(__instance, building.Map, building.Position, equip.PrimaryVerb);
                         }
