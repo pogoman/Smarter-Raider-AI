1e9f060230a337e127a56956d6ab9f01d5b4231a
trist <trist@LAPTOP-CP1EBFT0>
2025-08-11 23:00:31 +0200

    stage attack on damage

---
+++ Source/Pogoman/Patches/LordJob_StageThenAttack.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Linq;
+using Verse;
+using Verse.AI.Group;
+
+namespace PogoAI.Patches
+{
+    internal class LordJob_StageThenAttack
+    {
+        [HarmonyPatch(typeof(RimWorld.LordJob_StageThenAttack), "CreateGraph")]
+        static class LordJob_StageThenAttack_CreateGraph
+        {
+            static void Postfix(RimWorld.LordJob_StageThenAttack __instance, ref StateGraph __result)
+            {
+                foreach (var transition in __result.transitions)
+                {
+                    if (transition.target is RimWorld.LordToil_AssaultColonyBreaching)
+                    {
+                        transition.AddTrigger(new Trigger_PawnHarmed());
+                    }
+                }
+            }
+        }
+    }
+}
