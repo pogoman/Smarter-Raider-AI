0e19549012b29655416d73aa12f2ceffb331930e
trist <trist@LAPTOP-CP1EBFT0>
2025-08-14 10:19:59 +0200

    tweak triggers

--- Source/Pogoman/Patches/LordJob_Siege.cs
+++ Source/Pogoman/Patches/LordJob_Siege.cs
@@ -7,18 +7,19 @@ using Verse.AI.Group;
 
 namespace PogoAI.Patches
 {
-    internal class LordJob_StageThenAttack
+    internal class LordJob_Siege
     {
-        [HarmonyPatch(typeof(RimWorld.LordJob_StageThenAttack), "CreateGraph")]
-        static class LordJob_StageThenAttack_CreateGraph
+        [HarmonyPatch(typeof(RimWorld.LordJob_Siege), "CreateGraph")]
+        static class LordJob_Siege_CreateGraph
         {
-            static void Postfix(RimWorld.LordJob_StageThenAttack __instance, ref StateGraph __result)
+            static void Postfix(RimWorld.LordJob_Siege __instance, ref StateGraph __result)
             {
                 foreach (var transition in __result.transitions)
                 {
+                    Log.Message($"{transition.target}");
                     if (transition.target is RimWorld.LordToil_AssaultColonyBreaching)
                     {
-                        transition.AddTrigger(new Trigger_PawnHarmed(0.08f));
+                        transition.triggers.RemoveAll(t => t is Trigger_PawnHarmed);
                     }
                 }
             }
